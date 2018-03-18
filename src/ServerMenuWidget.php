29d913980eb7adf0c20ecf4e04f047da3082515b
macbook <luisarmando1234@gmail.com>
2018-03-18 09:35:44 -0600

    update

---
+++ src/ServerMenuWidget.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace inquid\panel;
+
+use yii\base\Widget;
+class ServerMenuWidget extends Widget
+{
+    public function init()
+    {
+    }
+    public function run()
+    {
+        return "Servers: ";
+    }
+}
